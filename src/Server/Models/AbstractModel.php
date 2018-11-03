6019f507caf1d37220c9db339c52d42b0c22edaf
Michał Szewczyk <ms@msworks.pl>
2018-11-03 13:30:25 +0100

    Added asArray method to AbstractModel

--- src/Server/Models/AbstractModel.php
+++ src/Server/Models/AbstractModel.php
@@ -10,6 +10,8 @@ declare(strict_types=1);
 
 namespace MS\RestServer\Server\Models;
 
+use MS\RestServer\Server\Exceptions\ResponseException;
+
 
 abstract class AbstractModel
 {
@@ -19,4 +21,35 @@ abstract class AbstractModel
      * @return array
      */
     public abstract function validate(): array;
+
+    /**
+     * @return array
+     * @throws ResponseException
+     */
+    public function asArray(): array
+    {
+        try {
+            $reflectionClass = new \ReflectionClass($this);
+            $classProperties = $reflectionClass->getProperties();
+            $data = [];
+
+            foreach ($classProperties as $classProperty) {
+                $propertyName = (string) $classProperty->getName();
+                $data[$propertyName] = $this->getPropertyValue($propertyName);
+            }
+
+            return $data;
+        } catch (\ReflectionException $exception) {
+            throw new ResponseException(500, $exception->getMessage());
+        }
+    }
+
+    /**
+     * @param $propertyName
+     * @return null
+     */
+    private function getPropertyValue($propertyName)
+    {
+        return property_exists($this, $propertyName) ? $this->$propertyName : null;
+    }
 }
